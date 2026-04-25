b7fde651c348c39b5dab3acdbf78d714246387b0
farahahmed27 <farahahmedkamal2004@gmail.com>
2026-04-25 13:21:11 +0300

    feat(playlists):add dto for delele playlist

--- src/playlists/dto/index.ts
+++ src/playlists/dto/index.ts
@@ -4,6 +4,7 @@ export { AddTrackToPlaylistDto } from './add-track-to-playlist.dto';
 export { ReorderPlaylistTracksDto } from './reorder-playlist-tracks.dto';
 export { PlaylistPaginationQueryDto } from './pagination-query.dto';
 export { GetPlaylistDetailsParamsDto } from './get-playlist-details-params.dto';
+export { DeletePlaylistParamsDto } from './delete-playlist-params.dto';
 export {
 	GetPlaylistDetailsResponseDto,
 	PlaylistDetailsOwnerDto,
