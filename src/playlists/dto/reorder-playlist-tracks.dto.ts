34006d99595b51df537e2aee32037eb4734f1f0e
farahahmed27 <farahahmedkamal2004@gmail.com>
2026-04-25 13:57:25 +0300

    feat(playlists): add dto for reorder playlist tracks

--- src/playlists/dto/reorder-playlist-tracks.dto.ts
+++ src/playlists/dto/reorder-playlist-tracks.dto.ts
@@ -1,3 +1,5 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsArray, IsNotEmpty, IsString } from 'class-validator';
 export class ReorderPlaylistTracksDto {
   orderedTrackIds!: string[];
 }
